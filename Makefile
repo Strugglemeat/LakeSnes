9b612b339bc1790c36faa9c6ad48f0b283e740e6
bankbank <36766617+Strugglemeat@users.noreply.github.com>
2023-03-19 21:35:29 +0800

    Add files via upload

--- Makefile
+++ Makefile
@@ -8,14 +8,12 @@ else
 	sdlflags = `sdl2-config --cflags --libs`
 endif
 
-CFLAGS = -O3 -I ./snes -I ./zip
+CFLAGS = -O3 -I ./snes
 
 execname = lakesnes
 
-cfiles = snes/spc.c snes/dsp.c snes/apu.c snes/cpu.c snes/dma.c snes/ppu.c snes/cart.c snes/input.c snes/snes.c snes/snes_other.c \
- zip/zip.c tracing.c main.c
-hfiles = snes/spc.h snes/dsp.h snes/apu.h snes/cpu.h snes/dma.h snes/ppu.h snes/cart.h snes/input.h snes/snes.h \
- zip/zip.h zip/miniz.h tracing.h
+cfiles = snes/spc.c snes/dsp.c snes/apu.c snes/cpu.c snes/dma.c snes/ppu.c snes/cart.c snes/input.c snes/snes.c snes/snes_other.c main.c
+hfiles = snes/spc.h snes/dsp.h snes/apu.h snes/cpu.h snes/dma.h snes/ppu.h snes/cart.h snes/input.h snes/snes.h
 
 .PHONY: all clean
 
--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,38 +6,17 @@
 
 #include <SDL2/SDL.h>
 
-#include "zip.h"
-
 #include "snes.h"
-#include "tracing.h"
-
-/* depends on behaviour:
-casting uintX_t to/from intX_t does 'expceted' unsigned<->signed conversion
-  ((int8_t) 255) == -1
-same with assignment
-  int8_t a; a = 0xff; a == -1
-overflow is handled as expected
-  (uint8_t a = 255; a++; a == 0; uint8_t b = 0; b--; b == 255)
-clipping is handled as expected
-  (uint16_t a = 0x123; uint8_t b = a; b == 0x23)
-giving non 0/1 value to boolean makes it 0/1
-  (bool a = 2; a == 1)
-giving out-of-range vaue to function parameter clips it in range
-  (void test(uint8_t a) {...}; test(255 + 1); a == 0 within test)
-int is at least 32 bits
-shifting into sign bit makes value negative
-  int a = ((int16_t) (0x1fff << 3)) >> 3; a == -1
-*/
 
 static uint8_t* readFile(const char* name, size_t* length);
 static bool loadRom(const char* name, Snes* snes);
-static void setTitle(SDL_Window* window, const char* path);
-static bool checkExtention(const char* name, bool forZip);
+
 static void playAudio(Snes* snes, SDL_AudioDeviceID device, int16_t* audioBuffer, int wantedSamples);
 static void renderScreen(Snes* snes, SDL_Renderer* renderer, SDL_Texture* texture);
 static void handleInput(Snes* snes, int keyCode, bool pressed);
 
 int main(int argc, char** argv) {
+
   // set up SDL
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
     printf("Failed to init SDL: %s\n", SDL_GetError());
@@ -60,6 +38,7 @@ int main(int argc, char** argv) {
     printf("Failed to create texture: %s\n", SDL_GetError());
     return 1;
   }
+
   SDL_AudioSpec want, have;
   SDL_AudioDeviceID device;
   SDL_memset(&want, 0, sizeof(want));
@@ -75,23 +54,22 @@ int main(int argc, char** argv) {
   }
   int16_t* audioBuffer = malloc(882 * 4); // *2 for stereo, *2 for sizeof(int16)
   SDL_PauseAudioDevice(device, 0);
+
   // print version
   SDL_version version;
   SDL_GetVersion(&version);
   printf("LakeSnes - Running with SDL %d.%d.%d\n", version.major, version.minor, version.patch);
+
   // init snes, load rom
   Snes* snes = snes_init();
   bool loaded = false;
-  if(argc >= 2) {
-    loaded = loadRom(argv[1], snes);
-    if(loaded) setTitle(window, argv[1]);
-  } else {
-    puts("No rom loaded");
-  }
+
+  if(argc >= 2) loaded = loadRom(argv[1], snes);
+  else puts("No rom loaded");
+
   // sdl loop
   bool running = true;
   bool paused = false;
-  bool runOne = false;
   bool turbo = false;
   SDL_Event event;
   int fullscreenFlags = 0;
@@ -109,36 +87,8 @@ int main(int argc, char** argv) {
           switch(event.key.keysym.sym) {
             case SDLK_r: snes_reset(snes, false); break;
             case SDLK_e: snes_reset(snes, true); break;
-            case SDLK_o: runOne = true; break;
             case SDLK_p: paused = !paused; break;
             case SDLK_t: turbo = true; break;
-            case SDLK_j: {
-              puts("Dumping to dump.bin...");
-              FILE* f = fopen("dump.bin", "wb");
-              fwrite(snes->ram, 0x20000, 1, f);
-              fwrite(snes->ppu->vram, 0x10000, 1, f);
-              fwrite(snes->ppu->cgram, 0x200, 1, f);
-              fwrite(snes->ppu->oam, 0x200, 1, f);
-              fwrite(snes->ppu->highOam, 0x20, 1, f);
-              fwrite(snes->apu->ram, 0x10000, 1, f);
-              fclose(f);
-              break;
-            }
-            case SDLK_l: {
-              // run one cpu cycle
-              snes_runCpuCycle(snes);
-              char line[80];
-              getProcessorStateCpu(snes, line);
-              puts(line);
-              break;
-            }
-            case SDLK_k: {
-              // TODO: spc cycling
-              // char line[57];
-              // getProcessorStateSpc(snes, line);
-              // puts(line);
-              break;
-            }
             case SDLK_RETURN: {
               if(event.key.keysym.mod & KMOD_ALT) {
                 fullscreenFlags ^= SDL_WINDOW_FULLSCREEN_DESKTOP;
@@ -164,15 +114,6 @@ int main(int argc, char** argv) {
           running = false;
           break;
         }
-        case SDL_DROPFILE: {
-          char* droppedFile = event.drop.file;
-          if(loadRom(droppedFile, snes)) loaded = true;
-          if(loaded) setTitle(window, droppedFile);
-          SDL_free(droppedFile);
-          wanted = 1.0 / (snes->palTiming ? 50.0 : 60.0);
-          wantedSamples = snes->palTiming ? 882 : 735;
-          break;
-        }
       }
     }
 
@@ -185,8 +126,7 @@ int main(int argc, char** argv) {
     while(timeAdder >= wanted - 0.002) {
       timeAdder -= wanted;
       // run frame
-      if(loaded && (!paused || runOne)) {
-        runOne = false;
+      if(loaded && !paused) {
         if(turbo) {
           snes_runFrame(snes);
         }
@@ -200,6 +140,7 @@ int main(int argc, char** argv) {
     SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer); // should vsync
   }
+
   // clean snes
   snes_free(snes);
   // clean sdl
@@ -211,6 +152,7 @@ int main(int argc, char** argv) {
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
+
 }
 
 static void playAudio(Snes* snes, SDL_AudioDeviceID device, int16_t* audioBuffer, int wantedSamples) {
@@ -249,68 +191,12 @@ static void handleInput(Snes* snes, int keyCode, bool pressed) {
   }
 }
 
-static bool checkExtention(const char* name, bool forZip) {
-  if(name == NULL) return false;
-  int length = strlen(name);
-  if(length < 4) return false;
-  if(forZip) {
-    if(strcmp(name + length - 4, ".zip") == 0) return true;
-    if(strcmp(name + length - 4, ".ZIP") == 0) return true;
-  } else {
-    if(strcmp(name + length - 4, ".smc") == 0) return true;
-    if(strcmp(name + length - 4, ".SMC") == 0) return true;
-    if(strcmp(name + length - 4, ".sfc") == 0) return true;
-    if(strcmp(name + length - 4, ".SFC") == 0) return true;
-  }
-  return false;
-}
-
 static bool loadRom(const char* name, Snes* snes) {
-  // zip library from https://github.com/kuba--/zip
   size_t length = 0;
-  uint8_t* file = NULL;
-  if(checkExtention(name, true)) {
-    struct zip_t* zip = zip_open(name, 0, 'r');
-    if(zip != NULL) {
-      int entries = zip_total_entries(zip);
-      for(int i = 0; i < entries; i++) {
-        zip_entry_openbyindex(zip, i);
-        const char* zipFilename = zip_entry_name(zip);
-        if(checkExtention(zipFilename, false)) {
-          printf("Read \"%s\" from zip\n", zipFilename);
-          zip_entry_read(zip, (void**) &file, &length);
-          break;
-        }
-        zip_entry_close(zip);
-      }
-      zip_close(zip);
-    }
-  } else {
-    file = readFile(name, &length);
-  }
-  if(file == NULL) {
-    puts("Failed to read file");
-    return false;
-  }
+  uint8_t* file = readFile(name, &length);
   bool result = snes_loadRom(snes, file, length);
   free(file);
   return result;
-}
-
-static void setTitle(SDL_Window* window, const char* path) {
-  // get last occurence of '/'
-  const char* filename = strrchr(path, '/');
-  if(filename == NULL) {
-    filename = path;
-  } else {
-    filename += 1; // skip past '/' itself
-  }
-  int length = strlen(filename);
-  char* title = malloc(length + 12); // "LakeSnes - ": 11 characters, +1 for '\0'
-  strcpy(title, "LakeSnes - ");
-  strcat(title, filename);
-  SDL_SetWindowTitle(window, title);
-  free(title);
 }
 
 static uint8_t* readFile(const char* name, size_t* length) {
